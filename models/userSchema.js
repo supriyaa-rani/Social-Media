0106a5d939826b4878e61d55fab5b4336a602daf
supriya <supriyarani173@gmail.com>
2024-07-14 11:47:43 +0530

    Database | UserSchema

---
+++ models/userSchema.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type:String,
+        trim:true,
+        required:[true, 'Full Name is required'],
+        minLength:[4, 'Full Name must be 4 characters long']
+    },
+    username: {
+        type:String,
+        trim:true,
+        unique:true,
+        required:[true, 'Username is required'],
+        minLength:[4, 'Username must be 4 characters long']
+    },
+    email: {
+        type:String,
+        trim:true,
+        required:[true, 'Email  is required'],
+        unique:true,
+        lowercase:true,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            "Please fill a valid email address",
+        ],
+    },
+    password:String,
+}, {timestamps:true})
+
+const User = mongoose.model('user', userSchema)
+
+module.exports = User
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const User = require('../models/userSchema')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
@@ -18,6 +20,14 @@ router.get('/register', function(req, res, next) {
   res.render('register');
 });
 
-
+router.post('/register-user',async function(req, res, next) {
+  try {
+    const newuser = new User(req.body)
+    await newuser.save()
+    res.redirect('/login');
+  } catch (error) {
+    res.send(error)
+  } 
+});
 
 module.exports = router;
